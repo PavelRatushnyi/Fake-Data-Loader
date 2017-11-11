5a0fd1d51fb44e3ace6035a50b64461dd9a04466
PavelRatushnyi <pavelratushniy@gmail.com>
2017-11-11 23:53:33 +0200

    Fix presenters

--- app/src/main/java/com/example/pavel/fakedataloader/mvp/views/ListView.java
+++ app/src/main/java/com/example/pavel/fakedataloader/mvp/views/ListView.java
@@ -1,8 +1,15 @@
 package com.example.pavel.fakedataloader.mvp.views;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndStrategy;
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.example.pavel.fakedataloader.mvp.models.Post;
 
+@StateStrategyType(AddToEndSingleStrategy.class)
 public interface ListView extends MvpView {
+
+	@StateStrategyType(OneExecutionStateStrategy.class)
 	void showDetails(Post post);
 }
--- app/src/main/java/com/example/pavel/fakedataloader/mvp/views/PostsView.java
+++ app/src/main/java/com/example/pavel/fakedataloader/mvp/views/PostsView.java
@@ -1,10 +1,13 @@
 package com.example.pavel.fakedataloader.mvp.views;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.example.pavel.fakedataloader.mvp.models.Post;
 
 import java.util.List;
 
+@StateStrategyType(AddToEndSingleStrategy.class)
 public interface PostsView extends MvpView {
 	void showLoadingProgress();
 
